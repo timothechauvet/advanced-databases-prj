6d50af2b4193daffa5edf6d99c16d791317c063a
timothecht <timothe.chauvet@efrei.net>
2021-01-16 17:57:42 +0100

    debugtable date working

--- project.sql
+++ project.sql
@@ -7,19 +7,18 @@ drop table GRANTS             CASCADE CONSTRAINTS;
 drop table TICKETS            CASCADE CONSTRAINTS;
 drop table CUSTOMER           CASCADE CONSTRAINTS;
 drop table REDUCE_RATE        CASCADE CONSTRAINTS;
-drop table DEBUG              CASCADE CONSTRAINTS;
+drop table DEBUGTABLE              CASCADE CONSTRAINTS;
 drop table ARCHIVE            CASCADE CONSTRAINTS;
 
 -- Edit date format
-
-ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY' ;
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY';
 
 -- Create tables
 /***************************** THEATER **********************************/
 
 CREATE TABLE THEATER_COMPANY
 (
-  _theater_company_id     NUMBER NOT NULL, /*ID*/
+  theater_company_id     NUMBER NOT NULL, /*ID*/
 
   hall_capacity           NUMBER NOT NULL,
   name_company            VARCHAR(64) NOT NULL;
@@ -27,14 +26,13 @@ CREATE TABLE THEATER_COMPANY
   city                    VARCHAR(64) NOT NULL,
   balance                 FLOAT NOT NULL
 
-  PRIMARY KEY (_theater_company_id),
+  PRIMARY KEY (theater_company_id),
   CONSTRAINT BUDGET_CSTR CHECK(budget >= 0)
 );
 
 CREATE OR REPLACE TRIGGER balance_update
-    AFTER UPDATE OF curr_date ON DEBUG
+    AFTER UPDATE OF curr_date ON DEBUGTABLE
     FOR EACH ROW
-    
     begin
       IF DAY(curr_date) = 1 THEN
         balance
@@ -45,15 +43,15 @@ CREATE OR REPLACE TRIGGER balance_update
 
 CREATE TABLE CREATIONS
 (
-  _creation_id            NUMBER NOT NULL,
+  creation_id            NUMBER NOT NULL,
 
   creation_cost           FLOAT NOT NULL,
   promotion_creations     FLOAT NOT NULL,
   
-  _theater_company_id     NUMBER NOT NULL,
+  theater_company_id     NUMBER NOT NULL,
 
-  PRIMARY KEY (_creation_id),
-  FOREIGN KEY (_theater_company_id) REFERENCES THEATER_COMPANY(_theater_company_id),
+  PRIMARY KEY (creation_id),
+  FOREIGN KEY (theater_company_id) REFERENCES THEATER_COMPANY(theater_company_id),
   /*This will be a factor multiplied with the price in order to apply promotions*/
   CONSTRAINT PROMO_CSTR CHECK(promotion_creations >= 0 AND promotion_creations <= 1) 
 );
@@ -62,7 +60,7 @@ CREATE TABLE CREATIONS
 
 CREATE TABLE REPRESENTATION
 (
-  _representation_id      NUMBER NOT NULL,
+  representation_id      NUMBER NOT NULL,
 
   date_representation     DATE NOT NULL,
   normal_reference_rate   FLOAT NOT NULL,
@@ -70,10 +68,10 @@ CREATE TABLE REPRESENTATION
   money_made              FLOAT NOT NULL,
   tickets_sold            NUMBER,
   
-  _creation_id            NUMBER NOT NULL,
+  creation_id            NUMBER NOT NULL,
 
-  PRIMARY KEY (_representation_id),
-  FOREIGN KEY (_creation_id) REFERENCES CREATIONS(_creation_id)
+  PRIMARY KEY (representation_id),
+  FOREIGN KEY (creation_id) REFERENCES CREATIONS(creation_id)
 );
 
 CREATE OR REPLACE TRIGGER no_duplicates_representation
@@ -93,47 +91,47 @@ CREATE OR REPLACE TRIGGER no_duplicates_representation
 
 CREATE TABLE GRANTS
 (
-  _grant_id     NUMBER NOT NULL,
+  grant_id     NUMBER NOT NULL,
 
   entity        VARCHAR(64) NOT NULL,
   amount        FLOAT NOT NULL,
   number_months NUMBER NOT NULL,
   date_start    DATE NOT NULL,
 
-  PRIMARY KEY (_grant_id)
+  PRIMARY KEY (grant_id)
 );
 
 /***************************** TICKETS **********************************/
 
 CREATE TABLE TICKETS
 (
-  _ticket_id          NUMBER NOT NULL,
+  ticket_id          NUMBER NOT NULL,
 
   price               FLOAT NOT NULL,
   promotion           FLOAT NOT NULL,
   buying_date         DATE NOT NULL,
   
-  _representation_id  NUMBER NOT NULL,
+  representation_id  NUMBER NOT NULL,
 
-  PRIMARY KEY (_ticket_id),
-  FOREIGN KEY (_representation_id) REFERENCES REPRESENTATION(_representation_id)
+  PRIMARY KEY (ticket_id),
+  FOREIGN KEY (representation_id) REFERENCES REPRESENTATION(representation_id)
 );
 
 CREATE TABLE CUSTOMER
 (
-  _customer_id    NUMBER NOT NULL,
+  customer_id    NUMBER NOT NULL,
 
   age             NUMBER NOT NULL,
   mail            VARCHAR(64) NOT NULL,
   name_customer   VARCHAR(64) NOT NULL,
   job             VARCHAR(64) NOT NULL,
 
-  PRIMARY KEY (_customer_id)
+  PRIMARY KEY (customer_id)
 );
 
 CREATE TABLE REDUCE_RATE
 (
-  _reduce_id              NUMBER NOT NULL,
+  reduce_id              NUMBER NOT NULL,
 
   age_reduce              NUMBER NOT NULL,
   job_reduce              VARCHAR(64) NOT NULL,
@@ -142,31 +140,75 @@ CREATE TABLE REDUCE_RATE
   finish_date             DATE NOT NULL,
   completion_percentage   FLOAT NOT NULL,
 
-  _representation_id      NUMBER NOT NULL,
+  representation_id      NUMBER NOT NULL,
 
-  PRIMARY KEY (_reduce_id),
-  FOREIGN KEY (_representation_id) REFERENCES REPRESENTATION(_representation_id)
+  PRIMARY KEY (reduce_id),
+  FOREIGN KEY (representation_id) REFERENCES REPRESENTATION(representation_id)
 );
 
 /***************************** OTHER **********************************/
 
-CREATE TABLE DEBUG
+CREATE TABLE DEBUGTABLE
 (
-  curr_date DATE NOT NULL
+  debugtable_pk NUMBER NOT NULL,
+
+  curr_date DATE NOT NULL,
+
+  PRIMARY KEY (debugtable_pk)
 );
+
+INSERT INTO DEBUGTABLE VALUES (1, '17-01-2021');
+
+/*Simple function to get the current date*/
+CREATE OR REPLACE FUNCTION getCurrDate
+RETURN Date IS
+  curr_date_return Date := '01-01-2000';
+begin
+  SELECT MAX(curr_date) INTO curr_date_return
+  FROM DEBUGTABLE;
+  
+  RETURN curr_date_return;
+end;
+
+/*Make it impossible to go back in time for DEBUGTABLE*/
+CREATE OR REPLACE TRIGGER timeTrigger
+  BEFORE UPDATE ON DEBUGTABLE
+  FOR EACH ROW
+  DECLARE
+    timeFctPreviousDate EXCEPTION;
+  
+  begin
+  IF :OLD.curr_date > :NEW.curr_date THEN
+    RAISE timeFctPreviousDate;
+  END IF;
+    EXCEPTION WHEN timeFctPreviousDate THEN
+    RAISE_APPLICATION_ERROR(-20002, 'The new date cannot be before the older one');
+  end;
+
+/*Make it impossible to insert or delete the DEBUGTABLE*/
+CREATE OR REPLACE TRIGGER timeException
+  BEFORE DELETE OR INSERT ON DEBUGTABLE
+  FOR EACH ROW
+  DECLARE
+    timeException EXCEPTION;
+  begin
+    RAISE timeException;
+    EXCEPTION WHEN timeException THEN
+    RAISE_APPLICATION_ERROR(-20003, 'You cannot insert or delete on DEBUGTABLE');
+  end;
 
 CREATE TABLE ARCHIVE
 (
-  _trans_id           NUMBER NOT NULL,
+  trans_id           NUMBER NOT NULL,
 
   date_transaction    DATE NOT NULL,
   amount              FLOAT NOT NULL,
   type_transaction    VARCHAR(16) NOT NULL,
 
-  _theater_company_id NUMBER NOT NULL,
+  theater_company_id NUMBER NOT NULL,
 
-  PRIMARY KEY (_trans_id),
-  FOREIGN KEY (_theater_company_id) REFERENCES THEATER_COMPANY(_theater_company_id)
+  PRIMARY KEY (trans_id),
+  FOREIGN KEY (theater_company_id) REFERENCES THEATER_COMPANY(theater_company_id)
 );
 
 /**************************** CONNECTION BETWEEN ENTITIES ***********************************/
@@ -175,11 +217,11 @@ CREATE TABLE IS_GIVEN
   date_given          DATE NOT NULL,
   amount_given        FLOAT NOT NULL,
 
-  _theater_company_id NUMBER NOT NULL,
-  _grant_id           NUMBER NOT NULL,
+  theater_company_id NUMBER NOT NULL,
+  grant_id           NUMBER NOT NULL,
 
-  FOREIGN KEY (_grant_id) REFERENCES GRANTS(_grant_id),
-  FOREIGN KEY (_theater_company_id) REFERENCES THEATER_COMPANY(_theater_company_id)
+  FOREIGN KEY (grant_id) REFERENCES GRANTS(grant_id),
+  FOREIGN KEY (theater_company_id) REFERENCES THEATER_COMPANY(theater_company_id)
 );
 
 CREATE TABLE HOSTS 
@@ -189,20 +231,20 @@ CREATE TABLE HOSTS
   traveling_cost      FLOAT NOT NULL,
   staging_cost        FLOAT NOT NULL,
 
-  _theater_company_id NUMBER NOT NULL,
-  _representation_id  NUMBER NOT NULL,
+  theater_company_id NUMBER NOT NULL,
+  representation_id  NUMBER NOT NULL,
 
-  FOREIGN KEY (_representation_id) REFERENCES REPRESENTATION(_representation_id),
-  FOREIGN KEY (_theater_company_id) REFERENCES THEATER_COMPANY(_theater_company_id)
+  FOREIGN KEY (representation_id) REFERENCES REPRESENTATION(representation_id),
+  FOREIGN KEY (theater_company_id) REFERENCES THEATER_COMPANY(theater_company_id)
 );
 
 CREATE TABLE BUYS 
 (
   buying_date   DATE NOT NULL,
 
-  _customer_id  NUMBER NOT NULL,
-  _ticket_id    NUMBER NOT NULL,
+  customer_id  NUMBER NOT NULL,
+  ticket_id    NUMBER NOT NULL,
 
-  FOREIGN KEY (_customer_id) REFERENCES CUSTOMER(_customer_id),
-  FOREIGN KEY (_ticket_id) REFERENCES TICKETS(_ticket_id)
+  FOREIGN KEY (customer_id) REFERENCES CUSTOMER(customer_id),
+  FOREIGN KEY (ticket_id) REFERENCES TICKETS(ticket_id)
 );
